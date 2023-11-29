1279e305786009351e339d48bff1f3935b12ecb3
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-29 13:52:25 +0400

    add a path to database_config.yaml

--- Constants/paths.py
+++ Constants/paths.py
@@ -4,3 +4,4 @@ CURRENT_FILE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 PROJECT_DIRECTORY = os.path.dirname(CURRENT_FILE_DIRECTORY)
 
 PATH_TO_RAW_ISS_INFO_JSON = os.path.join(PROJECT_DIRECTORY, 'RawData', 'raw_iss_info.json')
+PATH_TO_DATABASE_VARIABLES_YAML = os.path.join(PROJECT_DIRECTORY, 'database_config.yaml')
---
+++ RawData/save_raw_info.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import json
+from api_manager import ISSSatelliteDetails
+from Constants.paths import PATH_TO_RAW_ISS_INFO_JSON
+
+
+def save_raw_iss_info(iss_info: json) -> None:
+    # Read existing data from the file or create an empty DataFrame
+    try:
+        with open(PATH_TO_RAW_ISS_INFO_JSON, 'r') as json_file:
+            data = pd.read_json(json_file)
+    except FileNotFoundError:
+        data = pd.DataFrame()
+
+    # Convert the new response to a DataFrame and append it to the existing data
+    new_data = ISSSatelliteDetails().get_satellite_details()
+    data = data._append(new_data, ignore_index=True)
+
+    # Save the combined data to the JSON file
+    data.to_json(PATH_TO_RAW_ISS_INFO_JSON, orient='records', indent=2)
