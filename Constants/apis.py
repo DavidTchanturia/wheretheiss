f409d3df8e517f100e01aa3fc1d45cf98b28b634
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-29 17:30:33 +0400

    correct typo longtitude -> longitude

--- Constants/apis.py
+++ Constants/apis.py
@@ -1,5 +1,5 @@
 WHERE_IS_ISS_SATELLITES = "https://api.wheretheiss.at/v1/satellites"
 WHERE_IS_ISS_SATELLITES_DETAILS = "https://api.wheretheiss.at/v1/satellites/{satellite_id}"
 
-REVERSE_GEOCODING_API = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={latitude}&longitude={longtitude}&localityLanguage=en'
+REVERSE_GEOCODING_API = 'https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={latitude}&longitude={longitude}&localityLanguage=en'
 
