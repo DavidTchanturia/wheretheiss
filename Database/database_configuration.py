f4a294a998dff222346a7ffc042e80076e129734
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-30 23:00:22 +0400

    add loggers

--- Database/database_configuration.py
+++ Database/database_configuration.py
@@ -1,10 +1,22 @@
 import yaml
 from Constants.paths import PATH_TO_DATABASE_VARIABLES_YAML
+from Logger.iss_logger import setup_logging
+import logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
 
 
+# simply parse yaml to get database configruration details
 def parse_yaml(file_path=PATH_TO_DATABASE_VARIABLES_YAML):
-    with open(file_path, 'r') as file:
-        data = yaml.safe_load(file)
-        database_data = data["database"]
-
-    return database_data
+    try:
+        with open(file_path, 'r') as file:
+            data = yaml.safe_load(file)
+            database_data = data["database"]
+        return database_data
+    except FileNotFoundError as error:
+        logger.error(f"file can not be found on the path specified: {error}")
+    except yaml.YAMLError as error:
+        logger.error(f"yaml error: {error}")
+    except Exception as exception:
+        logger.error(f"exception occured while parsing yaml folder: {exception}")
