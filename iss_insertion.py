736f94c705e6fd7b6747e088aaa9ddfb3da90c13
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-30 23:03:30 +0400

    make ocmments clear

--- iss_insertion.py
+++ iss_insertion.py
@@ -3,9 +3,9 @@ import time
 
 
 normalized_data_for_insertion = ISSNormalizedDatabase()
-normalized_data_for_insertion.create_table_if_not_exists()
+normalized_data_for_insertion.create_table_if_not_exists()  # if the iss_normalized table does not exist, create
 
 while True:
     normalized_data_for_insertion = ISSNormalizedDatabase()
-    normalized_data_for_insertion.insert_into_normalized_table()
-    time.sleep(10)
+    normalized_data_for_insertion.insert_into_normalized_table()  # insert normalized data
+    time.sleep(10)  # data is inserted every 10 sec, I did it so its easier to check the task. Normally ti would be 5 min
--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from RawData.save_raw_info import ISSSatelliteDetails
+from Database.iss_wearhouse import ISSWarehouse
+from Database.DBManager import DatabaseConnector, ISSDataWarehouseManager
+from RawData.save_raw_info import save_raw_iss_info
+import time
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    db_connector = DatabaseConnector()
+
+    # if warehouse table does not exist, create one
+    data_warehouse_manager = ISSDataWarehouseManager(db_connector)
+    data_warehouse_manager.create_table()
+
+    # initialize object to get details later
+    dataframe = ISSSatelliteDetails()
+    save_raw_iss_info()  # save the raw info in json file
+    dataframe = dataframe.get_satellite_details()  # get iss details from wheretheiss api as pd dataframe
+
+    # modify data to be inserted in warehouse
+    warehouse_dataframe = ISSWarehouse(dataframe)
+    warehouse_dataframe.convert_timestamp_to_datetime()  # timestamp -> datetime
+    warehouse_dataframe.change_kilometer()  # changes kilometers -> km
+
+    # Insert data into the warehouse
+    warehouse_dataframe.insert_to_warehouse(db_connector)  # insert all the data
+
+    db_connector.close_connection() # the program will run while True, but I'll have close connection just in case
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    while True:
+        main()
+        time.sleep(2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
