8b52e0b0709a43de02b7f5fb1fdd4bc39ca805fb
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-30 14:34:07 +0400

    create ISS_25544_NORMALIZED_TABLE_SCHEMA

--- Constants/tables_schema.py
+++ Constants/tables_schema.py
@@ -1,3 +1,4 @@
+# schema for warehouse table
 ISS_25544_WAREHOUSE_SCHEMA = {
     'id': 'SERIAL PRIMARY KEY',
     'latitude': 'DECIMAL(10, 6) NOT NULL',
@@ -13,4 +14,14 @@ ISS_25544_WAREHOUSE_SCHEMA = {
     'units': "VARCHAR(2) NOT NULL"
 }
 
-ISS_25544_CLEANED_TABLE_SCHEMA = {}
+# schema for normalized table
+ISS_25544_NORMALIZED_TABLE_SCHEMA = {
+    'id': 'SERIAL PRIMARY KEY',
+    'latitude': 'DECIMAL(10, 6) NOT NULL',
+    'longitude': 'DECIMAL(10, 6) NOT NULL',
+    'visibility': 'VARCHAR(25) NOT NULL',
+    'date': 'TIMESTAMP NOT NULL',
+    'current_location': 'VARCHAR(255)',
+    'distance_travelled': 'DECIMAL(15, 8) NOT NULL',
+    "units": "VARCHAR(2)"
+}
