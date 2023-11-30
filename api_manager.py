41237dc529d7a3d787972e915323b9f17a5a1db1
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-30 23:02:58 +0400

    add loggers for common HTTP errors: 300, 400, 500

--- api_manager.py
+++ api_manager.py
@@ -1,6 +1,12 @@
 import requests
 import pandas as pd
 from Constants.apis import WHERE_IS_ISS_SATELLITES_DETAILS, WHERE_IS_ISS_SATELLITES, REVERSE_GEOCODING_API
+from Logger.iss_logger import setup_logging
+import logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
 
 class ISSSatelliteDetails:
     def __init__(self):
@@ -8,20 +14,36 @@ class ISSSatelliteDetails:
         self.details_df = None
 
     def get_satellite_id(self) -> int:
-        response = requests.get(WHERE_IS_ISS_SATELLITES)
-        response_json = response.json()
-        self.satellite_id = response_json[0].get("id")
-        return self.satellite_id
+        try:
+            response = requests.get(WHERE_IS_ISS_SATELLITES)
+            response_json = response.json()
+
+            # since most common errors are 300, 400, 500 I consider all of them
+            if response.status_code >= 300:
+                logger.error(f'HTTP error occured : {response.status_code} - {response.text}')
+
+            self.satellite_id = response_json[0].get("id")
+            return self.satellite_id
+        except Exception as exception:
+            logger.error(f'exception occured : {exception}')
+
 
     def get_satellite_details(self) -> pd.DataFrame:
-        if not self.satellite_id:
-            self.get_satellite_id()
+        try:
+            if not self.satellite_id:
+                self.get_satellite_id()
 
-        url = WHERE_IS_ISS_SATELLITES_DETAILS.format(satellite_id=self.satellite_id)
-        response = requests.get(url)
-        response_json = response.json()
+            url = WHERE_IS_ISS_SATELLITES_DETAILS.format(satellite_id=self.satellite_id)
+            response = requests.get(url)
 
-        # Use pandas to create a DataFrame from the JSON response
-        self.details_df = pd.json_normalize(response_json)
+            # same error handling implementation here as well
+            if response.status_code >= 300:
+                logger.error(f'API Error: {response.status_code} - {response.text}')
 
-        return self.details_df
+            response_json = response.json()
+
+            # create a dataframe form json
+            self.details_df = pd.json_normalize(response_json)
+            return self.details_df
+        except Exception as e:
+            logger.error(f'Error getting satellite details: {str(e)}')
