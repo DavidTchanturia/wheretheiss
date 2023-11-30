88b27a9066a28b0e10cc45b6c0bf7874dcdfd62a
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-30 14:32:43 +0400

    add comments to paths

--- Constants/paths.py
+++ Constants/paths.py
@@ -1,7 +1,10 @@
 import os
+# using this .py file to make all the paths in projects dynamic
 
+# get the absolute path to pwd parent directory
 CURRENT_FILE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
-PROJECT_DIRECTORY = os.path.dirname(CURRENT_FILE_DIRECTORY)
+PROJECT_DIRECTORY = os.path.dirname(CURRENT_FILE_DIRECTORY)  # in this case wheretheiss
 
+# add other inside paths to absolute path of wheretheiss
 PATH_TO_RAW_ISS_INFO_JSON = os.path.join(PROJECT_DIRECTORY, 'RawData', 'raw_iss_info.json')
 PATH_TO_DATABASE_VARIABLES_YAML = os.path.join(PROJECT_DIRECTORY, 'database_config.yaml')
