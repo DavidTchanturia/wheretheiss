7b121d1347e072f41793bed2a4fd54d697dddf8e
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-11-30 23:00:44 +0400

    add loggers

--- Database/DBManager.py
+++ Database/DBManager.py
@@ -1,6 +1,11 @@
 from Database.database_configuration import parse_yaml
 import psycopg2
 from Constants.queries import CREATE_ISS_WAREHOUSE_TABLE
+from Logger.iss_logger import setup_logging
+import logging
+
+setup_logging()
+logger = logging.getLogger(__name__)
 
 
 class DatabaseConnector:
@@ -10,12 +15,15 @@ class DatabaseConnector:
 
     # connect to the database
     def connect(self):
+        """connects to iss datbase"""
         try:
             parameters = parse_yaml()
             self.connection = psycopg2.connect(**parameters)
             self.cursor = self.connection.cursor()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
+        except psycopg2.DatabaseError as error:
+            logger.error(f"error while connecting: {error}")
+        except Exception as exception:
+            logger.error(f"exception occured while parsing yaml folder: {exception}")
 
     def close_connection(self):
         try:
@@ -23,9 +31,8 @@ class DatabaseConnector:
                 self.connection.close()
             if self.cursor:
                 self.cursor.close()
-        except Exception as error:
-            print(f"{error} encountered while closing connection")
-
+        except Exception as exception:
+            logger.error(f"{exception} encountered while closing connection")
 
 
 class ISSDataWarehouseManager:
@@ -38,5 +45,5 @@ class ISSDataWarehouseManager:
             self.db_connector.connect()
             self.db_connector.cursor.execute(CREATE_ISS_WAREHOUSE_TABLE)
             self.db_connector.connection.commit()
-        finally:
-            pass
+        except Exception as exception:
+            logger.error(f"An unexpected error occurred: {exception}")
