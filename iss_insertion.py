77189c81e51da1275de06bd4a8347415a3078858
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-01 12:01:12 +0400

    change 10 seconds wait to 5

--- iss_insertion.py
+++ iss_insertion.py
@@ -1,11 +1,13 @@
 from Database.iss_normalized_database import ISSNormalizedDatabase
 import time
 
-
+# needs to be executed once outside of the loop
 normalized_data_for_insertion = ISSNormalizedDatabase()
 normalized_data_for_insertion.create_table_if_not_exists()  # if the iss_normalized table does not exist, create
 
 while True:
     normalized_data_for_insertion = ISSNormalizedDatabase()
     normalized_data_for_insertion.insert_into_normalized_table()  # insert normalized data
-    time.sleep(10)  # data is inserted every 10 sec, I did it so its easier to check the task. Normally to would be 5 min
+
+    # modify as you see fit
+    time.sleep(5)  # data is inserted every 10 sec, however insertion is done using 5 minute chunks of table
