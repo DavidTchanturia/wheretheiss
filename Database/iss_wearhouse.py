b6e3c3adc62cf86a12cf8790df425f28181155da
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-01 11:58:32 +0400

    add logging and doc strings

--- Database/iss_wearhouse.py
+++ Database/iss_wearhouse.py
@@ -1,7 +1,8 @@
 import pandas as pd
-from Constants.queries import INSERT_ISS_INFO_WAREHOUSE, SELECT_ALL_FROM_ISS_WAREHOUSE, SELECT_DATA_IN_RANGE_QUERY
+from Constants.queries import INSERT_ISS_INFO_WAREHOUSE, SELECT_DATA_IN_RANGE_QUERY
 from Logger.iss_logger import setup_logging
 import logging
+from datetime import datetime
 
 setup_logging()
 logger = logging.getLogger(__name__)
@@ -11,7 +12,7 @@ class ISSWarehouse:
     def __init__(self, dataframe=None):
         self.dataframe = dataframe
 
-    def convert_timestamp_to_datetime(self, column_name='timestamp'):
+    def convert_timestamp_to_datetime(self, column_name='timestamp') -> pd.DataFrame:
         """Convert timestamp to datetime in the specified column"""
         try:
             if column_name in self.dataframe.columns:
@@ -22,37 +23,36 @@ class ISSWarehouse:
                 if 'daynum' in self.dataframe.columns:
                     self.dataframe['daynum'] = self.dataframe['daynum'].astype(float)
 
-                logger.info(f'Timestamp converted to datetime in column "{column_name}" successfully')
                 return self.dataframe
             else:
                 raise ValueError(f"Column '{column_name}' not found in the dataframe.")
         except Exception as exception:
             logger.error(f'Error converting timestamp to datetime: {exception}')
 
-    def change_kilometer(self):
+    def change_kilometer(self) -> None:
         """simply change kilometers to km"""
         self.dataframe["units"] = 'km'
 
-    def insert_to_warehouse(self, db_connector):
+    def insert_to_warehouse(self, db_connector) -> None:
         """insert DataFrame into the ISS warehouse table"""
         try:
             db_connector.connect()
-            #
+            # unpack values to insert
             records = [{k: v for k, v in record.items() if k not in ('name', 'id')} for record in self.dataframe.to_dict(orient='records')]
             db_connector.cursor.executemany(INSERT_ISS_INFO_WAREHOUSE, [tuple(record.values()) for record in records])
             db_connector.connection.commit()
 
-            print("Data inserted into warehouse")
+            print("Data inserted in iss_24455_warehouse")
         except Exception as exception:
             logger.error(f'exception while inserting data into warehouse: {exception}')
 
-    def select_data_in_range(self, five_minutes_ago, current_timestamp, db_connector):
+    def select_data_in_range(self, five_minutes_ago: datetime, current_timestamp: datetime, db_connector) -> tuple[pd.DataFrame, pd.DataFrame]:
         """two arguments are timestamp when the method is called and timestamp 5 min before
         based on that method selects all the data in that range"""
         try:
             db_connector.connect()
 
-            # Ensure start_timestamp and end_timestamp are in datetime format
+            # double check timestamp format
             start_timestamp = pd.to_datetime(five_minutes_ago, format='%Y-%m-%d %H:%M:%S')
             end_timestamp = pd.to_datetime(current_timestamp, format='%Y-%m-%d %H:%M:%S')
 
