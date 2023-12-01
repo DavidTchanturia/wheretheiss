c5edc591e204053461071866ef0ba9d6a6d3a78d
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-01 11:59:41 +0400

    add logging

--- GetData/get_iss_location.py
+++ GetData/get_iss_location.py
@@ -1,18 +1,18 @@
-import pandas as pd
 from Constants.apis import REVERSE_GEOCODING_API
-from api_manager import ISSSatelliteDetails
 import requests
+
 
 class ISSLocation:
     def __init__(self):
-        self.api_url = REVERSE_GEOCODING_API
+        self.api_url = REVERSE_GEOCODING_API  # this api uses lan and lon to give place name
 
-    def get_location(self, latitude, longitude) -> str:
+    def get_location(self, latitude: float, longitude: float) -> str:
         api_url = self.api_url.format(latitude=latitude, longitude=longitude)
         response = requests.get(api_url).json()
 
-        country = response.get("countryName")
+        country = response.get("countryName")  # get the country name that the iss is above of
         if not country:
+            # very often iss is on top of the ocean. so I get the ocean name if there is no country
             location = response.get("localityInfo")
             information = location.get("informative")[0]
             country = information.get("name")
