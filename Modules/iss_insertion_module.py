6a379b6933f54cd09ebec94d0919275b707bd9c6
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-03 14:18:15 +0400

    create run_iss_insertion_module() to insert data from warehouse to normalized table

--- Modules/iss_insertion_module.py
+++ Modules/iss_insertion_module.py
@@ -2,6 +2,9 @@ from Database.iss_normalized_database import ISSNormalizedDatabase
 import time
 
 def run_iss_insertion_module():
+    """this module is responsible for extracting data from warehouse,
+    transforming it and inserting into iss_normalized table"""
+
     # needs to be executed once outside of the loop
     normalized_data_for_insertion = ISSNormalizedDatabase()
     normalized_data_for_insertion.create_table_if_not_exists()  # if the iss_normalized table does not exist, create
@@ -10,5 +13,5 @@ def run_iss_insertion_module():
         normalized_data_for_insertion = ISSNormalizedDatabase()
         normalized_data_for_insertion.insert_into_normalized_table()  # insert normalized data
 
-        # modify as you see fit
-        time.sleep(300)  # data is inserted every 10 sec, however insertion is done using 5 minute chunks of table
+
+        time.sleep(300)  # will run every 5 minutes
