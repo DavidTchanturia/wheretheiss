b826abbf786472fba310eb8447baa96d05dcb5ee
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-03 14:17:36 +0400

    create run_json_module() to save data in raw json

--- Modules/json_module.py
+++ Modules/json_module.py
@@ -3,6 +3,9 @@ import time
 
 
 def run_json_module():
+    """this module is responsible for saving raw data into raw_iss_info.json
+
+    sends request to api every 2 seconds. because every second times out"""
     while True:
         save_raw_iss_info()
         time.sleep(2)
