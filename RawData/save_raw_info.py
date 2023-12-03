75cd6324a5001920d0507fe08872981369c64787
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-03 14:16:27 +0400

    modify save_raw_iss_info() to create json file in the beginning

--- RawData/save_raw_info.py
+++ RawData/save_raw_info.py
@@ -4,16 +4,16 @@ from Constants.paths import PATH_TO_RAW_ISS_INFO_JSON
 
 
 def save_raw_iss_info() -> None:
-    # Read existing data from the file or create an empty DataFrame
+    # create pandas dataframe of existing data in json file
     try:
         with open(PATH_TO_RAW_ISS_INFO_JSON, 'r') as json_file:
             data = pd.read_json(json_file)
     except FileNotFoundError:
-        data = pd.DataFrame()
+        data = pd.DataFrame() # create empty dataframe if there is no info in json
 
-    # Convert the new response to a DataFrame and append it to the existing data
+    # add new response to existing one
     new_data = ISSSatelliteDetails().get_satellite_details()
     data = data._append(new_data, ignore_index=True)  # just append did not work, using _append because of that
 
-    # Save the combined data to the JSON file
+    # add new data to json
     data.to_json(PATH_TO_RAW_ISS_INFO_JSON, orient='records', indent=2)
