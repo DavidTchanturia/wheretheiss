ef38c95213a1017424c67de538fcb59d9e28ec97
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-04 14:40:23 +0400

    call run_iss_insertion_module() function in the module

--- Modules/iss_insertion_module.py
+++ Modules/iss_insertion_module.py
@@ -13,5 +13,8 @@ def run_iss_insertion_module():
         normalized_data_for_insertion = ISSNormalizedDatabase()
         normalized_data_for_insertion.insert_into_normalized_table()  # insert normalized data
 
-
         time.sleep(300)  # will run every 5 minutes
+
+
+# set this up for bash script to run the function
+run_iss_insertion_module()
