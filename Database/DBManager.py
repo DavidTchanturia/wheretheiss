ee6d70d5f2d95bb92b98d22e314600afebc58d0e
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-07 15:55:48 +0400

    remove broad Exception block

--- Database/DBManager.py
+++ Database/DBManager.py
@@ -22,17 +22,14 @@ class DatabaseConnector:
             self.cursor = self.connection.cursor()
         except psycopg2.DatabaseError as error:
             logger.error(f"error while connecting: {error}")
-        except Exception as exception:
-            logger.error(f"exception occured while parsing yaml folder: {exception}")
+
 
     def close_connection(self):
-        try:
-            if self.connection:
-                self.connection.close()
-            if self.cursor:
-                self.cursor.close()
-        except Exception as exception:
-            logger.error(f"{exception} encountered while closing connection")
+        if self.connection:
+            self.connection.close()
+        if self.cursor:
+            self.cursor.close()
+
 
 
 class ISSDataWarehouseManager:
@@ -41,9 +38,6 @@ class ISSDataWarehouseManager:
 
     def create_table(self):
         """create movies table if it does not exist"""
-        try:
-            self.db_connector.connect()
-            self.db_connector.cursor.execute(CREATE_ISS_WAREHOUSE_TABLE)
-            self.db_connector.connection.commit()
-        except Exception as exception:
-            logger.error(f"An unexpected error occurred: {exception}")
+        self.db_connector.connect()
+        self.db_connector.cursor.execute(CREATE_ISS_WAREHOUSE_TABLE)
+        self.db_connector.connection.commit()
