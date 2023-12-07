1eacca5a103d42d7de8fc9edf1ef50698f4de435
DavidTchanturia <davit.tchanturia.@btu.edu.ge>
2023-12-07 14:54:45 +0400

    turn off date autoconvertion of pd.read_json()

--- RawData/save_raw_info.py
+++ RawData/save_raw_info.py
@@ -7,13 +7,15 @@ def save_raw_iss_info() -> None:
     # create pandas dataframe of existing data in json file
     try:
         with open(PATH_TO_RAW_ISS_INFO_JSON, 'r') as json_file:
-            data = pd.read_json(json_file)
+            # convert_date needs to be False, otherwise pandas will convert it automatically
+            data = pd.read_json(json_file, convert_dates=False)
     except FileNotFoundError:
-        data = pd.DataFrame() # create empty dataframe if there is no info in json
+        data = pd.DataFrame()  # create empty dataframe if there is no info in json
 
     # add new response to existing one
     new_data = ISSSatelliteDetails().get_satellite_details()
-    data = data._append(new_data, ignore_index=True)  # just append did not work, using _append because of that
+    frames_to_concat = [data, new_data]
+    data = pd.concat(frames_to_concat)  # use pandas concat instead of _append
 
     # add new data to json
     data.to_json(PATH_TO_RAW_ISS_INFO_JSON, orient='records', indent=2)
