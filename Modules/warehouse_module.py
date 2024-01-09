5b31674c805b28bc313d31da03bc273e8ed5a6bd
DavidTchanturia <davit.tchanturia.1@btu.edu.ge>
2024-01-09 16:36:06 +0400

    create table before running the module in While loop

--- Modules/warehouse_module.py
+++ Modules/warehouse_module.py
@@ -7,10 +7,11 @@ def run_warehouse_module():
     """this module is responsible for extracting data from raw json file and loading it into warehouse"""
     db_connector = DatabaseConnector()
     obj = JsonToWarehouse()
+    obj.create_iss_warehouse_table(db_connector)
 
     while True:
         obj.get_warehouse_data_wrapper(db_connector)
-        time.sleep(120)  # run every two minutes
+        time.sleep(30)  # run every two minutes
 
 
 # set this up for bash script to run the function
