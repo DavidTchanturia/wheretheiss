79482883f2db1c82dca9e91487d6afcf949a664c
DavidTchanturia <davit.tchanturia.1@btu.edu.ge>
2024-01-09 16:37:41 +0400

    remove redundant requirements installation command

--- main.sh
+++ main.sh
@@ -15,22 +15,18 @@ trap cleanup INT
 # include project path to avoid import errors
 export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT
 
-# install requirements
-echo "Installing dependencies..."
-pip install -r "$PROJECT_ROOT/requirements.txt"
-
 # start json module, which saves raw data
 echo "Starting json_module.py"
 python3 "$PROJECT_ROOT/Modules/json_module.py" &
 
 # wait for 120 seconds and start warehouse_module.py, which saves data from raw to warehouse
-sleep 120
+sleep 30
 echo "Starting warehouse_module.py"
 python3 "$PROJECT_ROOT/Modules/warehouse_module.py" &
 
 # In total, wait for 300 seconds and start iss_insertion_module.py
-# this module extracts, transforms adds some addition info and isnerts into iss_normalized table
-sleep 180
+# this module extracts, transforms adds some addition info and inserts into iss_normalized table
+sleep 60
 echo "Starting iss_insertion_module.py"
 python3 "$PROJECT_ROOT/Modules/iss_insertion_module.py" &
 wait
