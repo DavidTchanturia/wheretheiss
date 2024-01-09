af8e43e3689f3b5dcfeb984c3d11482474bef5ea
DavidTchanturia <davit.tchanturia.1@btu.edu.ge>
2024-01-09 16:35:33 +0400

    change time.sleep(1) -> time.sleep(2)

--- Modules/json_module.py
+++ Modules/json_module.py
@@ -8,7 +8,7 @@ def run_json_module():
     sends request to api every 2 seconds. because every second times out"""
     while True:
         save_raw_iss_info()
-        time.sleep(1)
+        time.sleep(2)
 
 
 # set this up for bash script to run the function
