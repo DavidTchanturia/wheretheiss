c874d2e1dbf379f023d05041099ca90bf93f0054
DavidTchanturia <davit.tchanturia.1@btu.edu.ge>
2024-01-09 16:34:59 +0400

    create warehouse before running the module in While loop

--- Modules/iss_insertion_module.py
+++ Modules/iss_insertion_module.py
@@ -4,12 +4,14 @@ import time
 def run_iss_insertion_module():
     """this module is responsible for extracting data from warehouse,
     transforming it and inserting into iss_normalized table"""
+    normalized_data_for_insertion = WarehouseToIssTable()
+    normalized_data_for_insertion.create_table_if_not_exists()
 
     while True:
         normalized_data_for_insertion = WarehouseToIssTable()
         normalized_data_for_insertion.insert_into_normalized_table()  # insert normalized data
 
-        time.sleep(300)  # will run every 5 minutes
+        time.sleep(60)  # will run every 5 minutes
 
 
 # set this up for bash script to run the function
